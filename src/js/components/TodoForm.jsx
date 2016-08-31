4aaf6df869c9d8c62b3be3b52faad55f39d155e3
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-08-31 20:57:56 +0900

    Remove state from component

--- src/js/components/TodoForm.jsx
+++ src/js/components/TodoForm.jsx
@@ -3,10 +3,6 @@ import React from 'react';
 export default class TodoForm extends React.Component {
   constructor() {
     super();
-    this.state = {
-      text: '',
-    };
-
     this.onSubmit = this.onSubmit.bind(this);
   }
 
--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
 
 export default class TodoList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: this.props.todos,
-    };
-  }
-
   render() {
-    const list = this.state.todos.map((todo, i) => <li key={i}>{todo.text}</li>);
+    const list = this.props.todos.map((todo, i) => <li key={i}>{todo.text}</li>);
 
     return (
       <ul>
